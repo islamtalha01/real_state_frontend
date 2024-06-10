cb11991d8ff824729add6556c12c361274dec15f
Muhammad Talha <islamtalha01@gmail.com>
2024-06-10 07:02:19 +0500

    Added Error handling on frontend

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {Provider} from "react-redux";
 import {store} from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import { MessageProvider } from './MessageContext';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
    <BrowserRouter>
+    {/* <ToastContainer> */}
    <MessageProvider>
       <App />
     </MessageProvider>
-   
+    <ToastContainer />
+
    </BrowserRouter>
    
 )
