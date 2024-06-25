1d03c26402f0a0fe0147bfaa6635b5e5063e89ad
Muhammad Talha <islamtalha01@gmail.com>
2024-06-25 06:01:24 +0500

    message text color edit

--- src/components/chat/SystemMessage.jsx
+++ src/components/chat/SystemMessage.jsx
@@ -1,3 +1,34 @@
+// import React from 'react';
+// import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
+// import Markdown from "react-markdown";
+// import Avatar from '../Avatar';
+// import ChatLoader from './ChatLoader';
+
+// const SystemMessage = ({ message, loading, isLastMessage }) => {
+//     return (
+//         <div className="bg-accents-0 text-white">
+//             <div className="max-w-4xl w-full mx-auto">
+//                 <div className="flex items-start gap-4 max-lg:px-2 py-4">
+//                     <Avatar className="h-8 w-8 text-gray-400" status='ai' />
+//                     <div className="flex flex-col">
+//                         <div className="font-bold">Agent</div>
+//                         {loading && isLastMessage ? (
+//                             <div className="mt-2 prose prose-invert">
+//                                 <ChatLoader />
+//                             </div>
+//                         ) : (
+//                             <div className="mt-2 prose prose-invert w-full">
+//                                 <Markdown>{message}</Markdown>
+//                             </div>
+//                         )}
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+// export default SystemMessage;
+
 import React from 'react';
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Markdown from "react-markdown";
@@ -17,7 +48,7 @@ const SystemMessage = ({ message, loading, isLastMessage }) => {
                                 <ChatLoader />
                             </div>
                         ) : (
-                            <div className="mt-2 prose prose-invert w-full">
+                            <div className="mt-2 prose prose-invert w-full hover:bg-white hover:text-black transition-colors duration-300">
                                 <Markdown>{message}</Markdown>
                             </div>
                         )}
@@ -27,4 +58,5 @@ const SystemMessage = ({ message, loading, isLastMessage }) => {
         </div>
     );
 };
+
 export default SystemMessage;
